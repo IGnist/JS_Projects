46cbee27963090d657a54f99e75113d5799acd9a
Sandeep Kumar <99944237+IGnist@users.noreply.github.com>
2024-03-27 09:35:13 +0000

    video3

---
+++ 02_Understanding.js
@@ -0,0 +1,13 @@
+const Uid=9844
+var Age=20
+let  Name="Sandeep"
+Number=8601631414
+console.table([Uid , Age , Name, Number])
+// UID=9099 Invalid as declared as const
+Age=22
+Name="Sandeep Kumar"
+Number=9009687414
+console.table([Uid , Age , Name, Number])
+var RealAge=String(Age)
+console.log(RealAge)
+console.log(typeof RealAge)
