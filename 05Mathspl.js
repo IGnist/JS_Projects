cd3bc82f9f8a60d2c6000a185da780d684ab2062
Sandeep Kumar <99944237+IGnist@users.noreply.github.com>
2024-03-29 17:51:08 +0000

    date n maths

---
+++ 05Mathspl.js
@@ -0,0 +1,17 @@
+let p=3.5654
+console.log(Math.floor(p))
+console.log(Math.abs(p))
+console.log(Math.ceil(p))
+let a=[3,5,9,1,55]
+console.log(Math.max(3,5,9,1,55))
+let Tarik=new Date;
+console.log(Tarik)
+console.log(Tarik.toString())
+console.log(Tarik.getDate())
+console.log(Tarik.toJSON())
+console.log(Tarik.toLocaleDateString())
+console.log(Tarik.toLocaleString())
+let Abhi=Date.now();
+let Ndate=new Date("01-14-2024")
+console.log(Math.floor(Abhi/3600000))
+console.log(Math.floor(Ndate.getTime()/3600000))
