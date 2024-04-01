ff8784a309fb8440508b89253b14f22ed173e6d6
Sandeep Kumar <99944237+IGnist@users.noreply.github.com>
2024-04-01 18:27:48 +0000

    passing in function

--- 09_function.js
+++ 09_function.js
@@ -12,3 +12,29 @@ function Welcome(Username){
     console.log(`welcome ${Username}`)
 }
 Welcome()
+function Looping(val1,val2,val3,...Num1){
+    return Num1
+}
+console.log(Looping(20,50,70,60,80))
+
+//object
+const Point={
+    Name:"Sandeep",
+    Age:22,
+    size:"M"
+}
+function Objectpas(Pointless){
+    console.log(   `welcome ${Pointless.Name} i know your age is ${Pointless.Age} and your shirt size is ${Pointless.size}`)
+}
+Objectpas(Point);
+Objectpas({
+    Name:"Ignist",
+    Age:5,
+    size:"xxl"});
+
+Array1=[22,55,88,99]    
+
+function ArrPas(Sum1){
+    return Sum1[0]
+}
+console.log(ArrPas(Array1))
